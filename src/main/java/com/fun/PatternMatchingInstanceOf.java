6abfcca34815fc78371233c53f3743f935c424f0
Asif A Fasih <aafasih@gmail.com>
2020-09-24 14:37:08 -0500

    adding an example for the pattern matching instanceof feature

---
+++ src/main/java/com/fun/PatternMatchingInstanceOf.java
@@ -0,0 +1,39 @@
+package com.fun;
+
+public class PatternMatchingInstanceOf {
+
+    public static void main(String[] args) {
+        Customer customer = new Customer();
+        Employee employee = new Employee();
+
+        applyAppropriateDiscount(customer);
+        applyAppropriateDiscount(employee);
+    }
+
+    private static void applyAppropriateDiscount(Person person) {
+        if (person instanceof Customer customer) {
+            customer.customerDiscount();
+        }
+        else if (person instanceof Employee employee) {
+            employee.employeeDiscount();
+        }
+    }
+
+
+
+    //Classes
+
+    abstract static class Person {}
+
+    final static class Employee extends Person {
+        void employeeDiscount() {
+            System.out.println("Employee discount applied!!!");
+        }
+    }
+
+    final static class Customer extends Person {
+        void customerDiscount() {
+            System.out.println("Customer discount applied!!!");
+        }
+    }
+}
